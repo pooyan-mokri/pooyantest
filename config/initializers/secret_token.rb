71912aa3650388d9ce979f754eecf745ca3c088b
Pooyan Mokri <pooyan.mokri@gmail.com>
2012-07-17 23:42:03 +0530

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pooyantest::Application.config.secret_token = '62fe2ad93ce5020fb5ec870f05383129998659d0499c9b5aa29ea5b8ff7cf43cb683c0acf666f5fb3ed65fd3e2d32bed1e8a6378523a7ddfdfaabf38c2cf4d43'
